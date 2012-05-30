a6c83c2bede757799a2878830d5d394b4b130df5
Michael Miller <mikemill@gmail.com>
2012-05-29 18:16:16 -0700

    Foreign keys should have delete/update actions set. Signed-off-by: Michael Miller <mikemill@gmail.com>

--- tabledefs/MySQL/usersettings.sql
+++ tabledefs/MySQL/usersettings.sql
@@ -1,8 +1,8 @@
- CREATE TABLE `usersettings` (
+CREATE TABLE `usersettings` (
   `id_user` int(10) unsigned NOT NULL,
   `field` varchar(20) NOT NULL,
   `value` varchar(100) NOT NULL,
   PRIMARY KEY (`id_user`,`field`),
   KEY `field` (`field`),
-  CONSTRAINT `userprofile_ibfk_1` FOREIGN KEY (`id_user`) REFERENCES `users` (`id_user`)
+  CONSTRAINT `usersettings_ibfk_1` FOREIGN KEY (`id_user`) REFERENCES `users` (`id_user`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8
